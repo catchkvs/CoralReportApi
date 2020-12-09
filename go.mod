5d5251781c4baa3a47d7fca6c86665ec208e7d4f
Sushil Singh <sushils@amazon.com>
2020-12-08 23:11:48 -0500

    Add support for scheduled queries

--- go.mod
+++ go.mod
@@ -6,8 +6,8 @@ require (
 	cloud.google.com/go/bigquery v1.11.2
 	//github.com/cockroachdb/pebble v0.0.0-20201007144542-b79d619f4761 // indirect
 	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/go-co-op/gocron v0.3.3 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/magiconair/properties v1.8.4
 	google.golang.org/api v0.32.0
-
 )
---
+++ pkg/report/cron.go
@@ -0,0 +1,42 @@
+package report
+
+import (
+	"github.com/go-co-op/gocron"
+	"log"
+	"time"
+)
+
+func Schedule(scheduledQuery ScheduledQuery)  {
+	cron := scheduledQuery.Cron
+	s1 := gocron.NewScheduler(time.UTC)
+	s1.StartAsync()
+	switch cron {
+	case "MONTHLY":
+		_, err := s1.Every(1).Month(1).Do(GenerateCurrentMonthReport, scheduledQuery.Query)
+		if err != nil {
+			log.Fatalf("Error add query to schedule. Query %s %s", scheduledQuery.Query.Id, err.Error())
+		}
+	case "WEEKLY":
+		_, err := s1.Every(1).Week().Do(GenerateCurrentMonthReport, scheduledQuery.Query)
+		if err != nil {
+			log.Fatalf("Error add query to schedule. Query %s %s", scheduledQuery.Query.Id, err.Error())
+		}
+	case "DAILY":
+		_, err := s1.Every(1).Day().Do(GenerateCurrentMonthReport, scheduledQuery.Query)
+		if err != nil {
+			log.Fatalf("Error add query to schedule. Query %s %s", scheduledQuery.Query.Id, err.Error())
+		}
+	case "HOURLY":
+		_, err := s1.Every(1).Hour().Do(GenerateCurrentMonthReport, scheduledQuery.Query)
+		if err != nil {
+			log.Fatalf("Error add query to schedule. Query %s %s", scheduledQuery.Query.Id, err.Error())
+		}
+	case "MINUTE":
+		_, err := s1.Every(1).Minute().Do(GenerateCurrentMonthReport, scheduledQuery.Query)
+		if err != nil {
+			log.Fatalf("Error add query to schedule. Query %s %s", scheduledQuery.Query.Id, err.Error())
+		}
+	default:
+		log.Fatalf("Schedule %s not supported", cron)
+	}
+}
